19df90e001c897b5c3ebf04dc6c3f246ad3d4d23
markpernia <perniamanila@gmail.com>
2024-01-18 11:42:25 +0100

    SMA-4: Initial Commit

---
+++ Backend/sportsmatch/src/main/java/com/sportsmatch/SportsmatchApplication.java
@@ -0,0 +1,13 @@
+package com.sportsmatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsmatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportsmatchApplication.class, args);
+    }
+
+}

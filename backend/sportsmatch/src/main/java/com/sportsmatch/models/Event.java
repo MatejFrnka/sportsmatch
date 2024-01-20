ae7424a9d65f866515fae15dfea3cb59760ee16d
ivamach <ivana.machacikova@gmail.com>
2024-01-20 18:00:12 +0100

    SMA-11: deleted unused import

--- backend/sportsmatch/src/main/java/com/sportsmatch/models/Event.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/models/Event.java
@@ -6,7 +6,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.HashSet;
 import java.util.Set;

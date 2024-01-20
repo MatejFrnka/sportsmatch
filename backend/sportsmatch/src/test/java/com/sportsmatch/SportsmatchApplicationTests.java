f3e07aec3bd8225b66e90571cf1f12e9238fae3d
markpernia <134836290+markpernia@users.noreply.github.com>
2024-01-20 10:00:52 +0100

    SMA-4: initialize java spring boot application (#3)

---
+++ backend/sportsmatch/src/test/java/com/sportsmatch/SportsmatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsmatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsmatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

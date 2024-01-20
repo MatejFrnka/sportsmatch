e826ec6d9cdb3f3c8cf84e166251baa849f00fa9
JanaDupcakova <jana.dupcakova@gmail.com>
2024-01-20 23:38:24 +0100

    SMA-7: Added swagger dependencies and enable swagger

--- backend/sportsmatch/src/main/java/com/sportsmatch/SportsmatchApplication.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/SportsmatchApplication.java
@@ -1,9 +1,14 @@
 package com.sportsmatch;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
 
 @SpringBootApplication
+@OpenAPIDefinition
+@EnableWebMvc
 public class SportsmatchApplication {
 
     public static void main(String[] args) {

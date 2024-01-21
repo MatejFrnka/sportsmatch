d08c8bdd12f32da0271db990410dc1928beba4c2
JanaDupcakova <jana.dupcakova@gmail.com>
2024-01-21 19:07:01 +0100

    solved case if the event is not found by id

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/EventsController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/EventsController.java
@@ -1,7 +1,10 @@
 package com.sportsmatch.controllers;
 
 import com.sportsmatch.dtos.EventDTO;
+import com.sportsmatch.models.Event;
 import com.sportsmatch.services.EventService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,19 +15,27 @@ public class EventsController {
     private EventService eventService;
 
 
-    public EventsController(EventService eventService
-                            ) {
+    public EventsController(EventService eventService) {
         this.eventService = eventService;
 
     }
 
     @GetMapping("/event/{id}")
     public ResponseEntity<?> getEvent(@PathVariable ("id") Long id){
-        EventDTO eventDTO = new EventDTO(eventService.getEventById(id));
-        return ResponseEntity.ok().body(eventDTO);
+        try {
+            Event event = eventService.getEventById(id);
+            if(event != null) {
+                EventDTO eventDTO = new EventDTO(event);
+                return ResponseEntity.ok().body(eventDTO);
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Event not found");
+            }
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid data");
+        }
     }
-//      todo Post
-//    @PostMapping("/v1/event")
+//     todo Post
+//    @PostMapping("/event")
 //    public ResponseEntity<?> addEvent(){
 //
 //        return new ResponseEntity<>();

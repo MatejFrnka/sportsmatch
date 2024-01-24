52588f9bc2158d2905440574874deb8f031bd978
markpernia <perniamanila@gmail.com>
2024-01-24 20:52:55 +0100

    SMA-3: added SecurityConfig class and added securityFilterChain implementation

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/AuthConfig.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/AuthConfig.java
@@ -4,8 +4,12 @@ import com.sportsmatch.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -19,5 +23,18 @@ public class AuthConfig {
         userRepository
             .findByEmail(username)
             .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+  }
+
+  @Bean
+  public AuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    authProvider.setUserDetailsService(userDetailsService());
+    authProvider.setPasswordEncoder(passwordEncoder());
+    return authProvider;
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }

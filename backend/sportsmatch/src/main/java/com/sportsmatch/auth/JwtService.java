03725c4f24dbf0c97ed2c6b45508fa198ecc65a5
markpernia <perniamanila@gmail.com>
2024-01-24 18:59:00 +0100

    SMA-3: Added auth package, created JwtAuthFilter and JwtService classes

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtService.java
@@ -0,0 +1,10 @@
+package com.sportsmatch.auth;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService {
+    public String extractUserName(String token) {
+        return null;
+    }
+}

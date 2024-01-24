655f7ae59635623fcab8be8096254d33f87b097d
markpernia <perniamanila@gmail.com>
2024-01-24 21:34:38 +0100

    SMA-3: added filter to allow h2 console by spring security

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/SecurityConfig.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/SecurityConfig.java
@@ -7,9 +7,11 @@ import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import static org.springframework.boot.autoconfigure.security.servlet.PathRequest.toH2Console;
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @Configuration
@@ -18,7 +20,8 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 public class SecurityConfig {
 
     private static final String[] WHITE_LIST_URL = {
-            "/api/v1/auth/**"
+            "/api/v1/auth/**",
+            "/h2-console/**"
             // add endpoints that are not authenticated
             };
     private final JwtAuthFilter jwtAuthFilter;
@@ -29,6 +32,9 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception { // set which endpoints are authenticated and not
         http
                 .csrf(AbstractHttpConfigurer::disable)
+                .csrf(c -> c.ignoringRequestMatchers(toH2Console()))
+                .authorizeHttpRequests(r -> r.requestMatchers(toH2Console()).permitAll())
+                .headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
                 .authorizeHttpRequests(r -> r.requestMatchers(WHITE_LIST_URL)
                         .permitAll()
                         .anyRequest().authenticated())

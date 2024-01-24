643b5979ef9af327becc4cda33d50b6fab9ee003
markpernia <perniamanila@gmail.com>
2024-01-24 19:14:00 +0100

    SMA-3: Added JWT secret key in application properties

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtService.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtService.java
@@ -1,9 +1,13 @@
 package com.sportsmatch.auth;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JwtService {
+
+    @Value("${jwt.secret}")
+    private String SECRET_KEY;
     public String extractUserName(String token) {
         return null;
     }

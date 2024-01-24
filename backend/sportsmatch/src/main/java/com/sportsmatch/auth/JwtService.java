758188dd04e79e8c60c70191322ad5d9bc34c428
markpernia <perniamanila@gmail.com>
2024-01-24 19:16:46 +0100

    SMA-3: Implemented extractAllClaims and getVerificationKey

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtService.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtService.java
@@ -1,14 +1,34 @@
 package com.sportsmatch.auth;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import javax.crypto.SecretKey;
 
 @Service
 public class JwtService {
 
-    @Value("${jwt.secret}")
-    private String SECRET_KEY;
-    public String extractUserName(String token) {
-        return null;
-    }
+  @Value("${jwt.secret}")
+  private String SECRET_KEY;
+
+  public String extractUserName(String token) {
+    return null;
+  }
+
+  private Claims extractAllClaims(String token) { // Takes all the claims from JWT Token
+    return Jwts.parser()
+        .verifyWith(getVerificationKey())
+        .build()
+        .parseSignedClaims(token)
+        .getPayload();
+  }
+
+  private SecretKey getVerificationKey() { // Sign JWT Token base from secret
+    byte[] keyBytes = Decoders.BASE64URL.decode(SECRET_KEY);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
 }

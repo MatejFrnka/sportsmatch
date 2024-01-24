5047dbc54db3d3c8801797611bd3330e084d2772
markpernia <perniamanila@gmail.com>
2024-01-24 20:54:30 +0100

    SMA-3: added bean of AuthenticationManager

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/AuthConfig.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/AuthConfig.java
@@ -4,8 +4,10 @@ import com.sportsmatch.repos.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -31,6 +33,11 @@ public class AuthConfig {
     authProvider.setUserDetailsService(userDetailsService());
     authProvider.setPasswordEncoder(passwordEncoder());
     return authProvider;
+  }
+
+  @Bean
+  public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+      return config.getAuthenticationManager();
   }
 
   @Bean

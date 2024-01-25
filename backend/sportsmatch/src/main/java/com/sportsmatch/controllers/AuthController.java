7ff7c18b32f32296b201676ec3ee07a92689fad0
markpernia <perniamanila@gmail.com>
2024-01-25 13:38:15 +0100

    SMA-3: added POST endpoints for register and authenticate/login

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
@@ -0,0 +1,28 @@
+package com.sportsmatch.controllers;
+
+import com.sportsmatch.auth.AuthService;
+import com.sportsmatch.dtos.RequestDTO;
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+
+  private final AuthService authService;
+
+  @PostMapping("/register")
+  public ResponseEntity<?> register(@RequestBody RequestDTO requestDTO) {
+    return ResponseEntity.ok(authService.register(requestDTO));
+  }
+
+  @PostMapping("/authenticate")
+  public ResponseEntity<?> authenticate(@RequestBody RequestDTO requestDTO) {
+    return ResponseEntity.ok(authService.authenticate(requestDTO));
+  }
+}

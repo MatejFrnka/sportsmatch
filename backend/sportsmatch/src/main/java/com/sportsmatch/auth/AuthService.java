4dc4eb7a5937c76ea1e68a8ea9458e83023fec2c
markpernia <perniamanila@gmail.com>
2024-01-25 13:35:45 +0100

    SMA-3: Added AuthService class and implement register and authenticate

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/AuthService.java
@@ -0,0 +1,49 @@
+package com.sportsmatch.auth;
+
+import com.sportsmatch.dtos.RequestDTO;
+import com.sportsmatch.mappers.UserMapper;
+import com.sportsmatch.models.Role;
+import com.sportsmatch.models.User;
+import com.sportsmatch.repos.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.regex.Pattern;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService {
+
+  private final UserMapper userMapper;
+  private final JwtService jwtService;
+  private final UserRepository userRepository;
+  private final AuthenticationManager authenticationManager;
+
+  public RequestDTO register(RequestDTO requestDTO) {
+    User user = userMapper.registerToUser(requestDTO);
+    if (user == null) {
+      return RequestDTO.builder().status("Invalid User Data").build();
+    }
+    user.setUsername("myUserName");
+    userRepository.save(user);
+    return RequestDTO.builder()
+        .email(requestDTO.getEmail())
+        .status("Registered Successfully")
+        .build();
+  }
+
+  public RequestDTO authenticate(RequestDTO requestDTO) {
+    authenticationManager.authenticate(
+        new UsernamePasswordAuthenticationToken(requestDTO.getEmail(), requestDTO.getPassword()));
+    User user = userRepository.findByEmail(requestDTO.getEmail()).orElseThrow();
+    String jwtToken = jwtService.generateToken(user);
+    return RequestDTO.builder()
+        .email(requestDTO.getEmail())
+        .status("Login Successfully")
+        .token(jwtToken)
+        .build();
+  }
+}

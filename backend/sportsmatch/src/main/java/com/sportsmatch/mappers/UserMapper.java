aa7f60c8088340a2780013ec3744393ca0bb24d8
markpernia <perniamanila@gmail.com>
2024-01-26 01:19:57 +0100

    SMA-3: Remove manual validation

--- backend/sportsmatch/src/main/java/com/sportsmatch/mappers/UserMapper.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/mappers/UserMapper.java
@@ -1,13 +1,11 @@
 package com.sportsmatch.mappers;
 
-import com.sportsmatch.dtos.RequestDTO;
+import com.sportsmatch.dtos.AuthRequestDTO;
 import com.sportsmatch.models.Role;
 import com.sportsmatch.models.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
-
-import java.util.regex.Pattern;
 
 @Component
 @RequiredArgsConstructor
@@ -15,30 +13,11 @@ public class UserMapper {
 
   private final PasswordEncoder passwordEncoder;
 
-  public User registerToUser(RequestDTO requestDTO) {
-
-    if (isNullEmailOrPassword(requestDTO) || isInvalidEmail(requestDTO.getEmail())) {
-      return null;
-    }
-
+  public User registerToUser(AuthRequestDTO authRequestDTO) {
     return User.builder()
-        .email(requestDTO.getEmail())
-        .password(passwordEncoder.encode(requestDTO.getPassword()))
+        .email(authRequestDTO.getEmail())
+        .password(passwordEncoder.encode(authRequestDTO.getPassword()))
         .role(Role.USER)
         .build();
-  }
-
-  // todo delete this check
-  private boolean isNullEmailOrPassword(RequestDTO requestDTO) {
-    return requestDTO.getEmail() == null
-        || requestDTO.getPassword() == null
-        || requestDTO.getPassword().trim().isEmpty();
-  }
-
-  // todo use @Email annotation
-  private boolean isInvalidEmail(String email) {
-    String regexPattern =
-        "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
-    return !Pattern.compile(regexPattern).matcher(email).matches();
   }
 }

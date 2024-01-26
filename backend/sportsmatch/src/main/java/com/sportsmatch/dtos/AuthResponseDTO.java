12bc759979ce4f35676744b1efdca044c94abf09
markpernia <perniamanila@gmail.com>
2024-01-26 01:17:50 +0100

    SMA-3: Created AuthResponseDTO

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/dtos/AuthResponseDTO.java
@@ -0,0 +1,15 @@
+package com.sportsmatch.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthResponseDTO {
+
+    private String token;
+}

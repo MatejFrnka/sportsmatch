31c4e606a9305e4ca59dd2fbc4adf46661aae941
markpernia <perniamanila@gmail.com>
2024-01-26 01:32:59 +0100

    SMA-3: Applied validation library

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
@@ -1,10 +1,12 @@
 package com.sportsmatch.controllers;
 
 import com.sportsmatch.auth.AuthService;
-import com.sportsmatch.dtos.RequestDTO;
+import com.sportsmatch.dtos.AuthRequestDTO;
+import com.sportsmatch.services.ValidationService;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,15 +18,40 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
   private final AuthService authService;
+  private final ValidationService validationService;
 
-  //todo add @Valid to work with validation annotations
+  /**
+   *
+   * @param authRequestDTO The authentication request containing user details.
+   * @param bindingResult The result of the validation process.
+   * @return ResponseEntity indicating the success or failure of the registration.
+   *         Returns a 400 Bad Request with validation errors (List<String>) if input is invalid.
+   *         Returns a 200 OK if registration is successful.
+   */
   @PostMapping("/register")
-  public ResponseEntity<?> register(@RequestBody RequestDTO requestDTO) {
-    return ResponseEntity.ok(authService.register(requestDTO));
+  public ResponseEntity<?> register(
+      @RequestBody @Valid AuthRequestDTO authRequestDTO, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return ResponseEntity.badRequest().body(validationService.getAllErrors(bindingResult));
+    }
+    authService.register(authRequestDTO);
+    return ResponseEntity.ok().build();
   }
 
+  /**
+   *
+   * @param authRequestDTO The authentication request containing user credentials.
+   * @param bindingResult The result of the validation process.
+   * @return ResponseEntity indicating the success or failure of the authentication.
+   *         Returns a 400 Bad Request with validation errors if input is invalid.
+   *         Returns a 200 OK with authentication details if authentication is successful.
+   */
   @PostMapping("/authenticate")
-  public ResponseEntity<?> authenticate(@RequestBody RequestDTO requestDTO) {
-    return ResponseEntity.ok(authService.authenticate(requestDTO));
+  public ResponseEntity<?> authenticate(
+      @RequestBody @Valid AuthRequestDTO authRequestDTO, BindingResult bindingResult) {
+    if (bindingResult.hasErrors()) {
+      return ResponseEntity.badRequest().body(validationService.getAllErrors(bindingResult));
+    }
+    return ResponseEntity.ok(authService.authenticate(authRequestDTO));
   }
 }

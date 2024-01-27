fc267da5b952ed751079aa23e549e051606e6120
Ivana Machacikova <131284389+ivamach@users.noreply.github.com>
2024-01-27 16:29:53 +0100

    Sma 29 set up test environment for the front end (#15)

--- frontend/sportsmatch-app/vite.config.ts
+++ frontend/sportsmatch-app/vite.config.ts
@@ -1,7 +1,19 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: './src/test/setup.ts',
+    //configuration from https://github.com/vitest-dev/vitest/blob/main/examples/react-testing-lib/vite.config.ts
+    //css might be disabled if we are not 
+    //going to hove test that rely on CSS
+    css: true,
+  }
 })

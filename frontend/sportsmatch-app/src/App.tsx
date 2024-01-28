e23dd5b70ea73e28753de966c72b05c52d07945b
ivamach <ivana.machacikova@gmail.com>
2024-01-28 14:00:42 +0100

    SMA-31: lint errors fixed

--- frontend/sportsmatch-app/src/App.tsx
+++ frontend/sportsmatch-app/src/App.tsx
@@ -1,29 +1,24 @@
-import {
-  BrowserRouter,
-  Route,  
-  Routes,  
-} from "react-router-dom";
-import "./App.css";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Test from "./pages/Test";
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Test from './pages/Test'
 
 function App() {
   return (
     <BrowserRouter>
-    <div>
-    <Routes>
-      <Route path="login" element={<Login />} />
-      <Route path="test" element={<Test />} >
-        <Route index element={<Test/>} />
-        <Route path=":testId" element={<Test/>} />
-        </Route>
-      <Route path="/" element={<Home />} />
-    </Routes>
-    </div>
+      <div>
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="test" element={<Test />}>
+            <Route index element={<Test />} />
+            <Route path=":testId" element={<Test />} />
+          </Route>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  );
+  )
 }
 
-export default App;
+export default App
--- frontend/sportsmatch-app/src/components/SportEvent.tsx
+++ frontend/sportsmatch-app/src/components/SportEvent.tsx
@@ -1,40 +1,43 @@
-import '../styles/SportEvent.css';
+import '../styles/SportEvent.css'
 
 interface SportEventProps {
-    event: {
-        id: number;
-        maxElo: number;
-        minElo: number;
-        dateEnd: string;
-        dateStart: string;
-        location: string;
-        title: string;
-        sport: string;
-        playerOne: string;
-        playerTwo?: string;
-
-    }
+  event: {
+    id: number
+    maxElo: number
+    minElo: number
+    dateEnd: string
+    dateStart: string
+    location: string
+    title: string
+    sport: string
+    playerOne: string
+    playerTwo?: string
+  }
 }
 
 function SportEvent({ event }: SportEventProps) {
-    return (
-        <>
-            <div className="container-sm">
-                <div className="event-card">
-                    <div className="left">
-                    <ul>
-                        <li>📍{event.location}</li>
-                        <li>🏅{event.minElo} - {event.maxElo}</li>
-                        <li>📆{event.dateStart} to {event.dateEnd}</li>
-                    </ul>
-                    </div>
-                    <div className="right">
-                    <h3>{event.title}</h3>
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="container-sm">
+        <div className="event-card">
+          <div className="left">
+            <ul>
+              <li>📍{event.location}</li>
+              <li>
+                🏅{event.minElo} - {event.maxElo}
+              </li>
+              <li>
+                📆{event.dateStart} to {event.dateEnd}
+              </li>
+            </ul>
+          </div>
+          <div className="right">
+            <h3>{event.title}</h3>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
-export default SportEvent;
+export default SportEvent

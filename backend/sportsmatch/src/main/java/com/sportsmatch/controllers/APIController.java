d21fcdf6d93653cbc859e1108c9ef8002c9d7284
markpernia <perniamanila@gmail.com>
2024-01-28 11:12:29 +0100

    SMA-30: added swagger annotation to authController instead of Javadoc comments

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/APIController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/APIController.java
@@ -1,6 +1,7 @@
 package com.sportsmatch.controllers;
 
 import com.sportsmatch.models.User;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class APIController {
 
-  @GetMapping("/hello")
-  public String hello(Authentication authentication) {
-    User user = (User) authentication.getPrincipal();
-    return "Welcome " + user.getName() + " to Secured Endpoint ";
-  }
+    @GetMapping("/hello")
+    @Tag(name = "ex.secured endpoint")
+    public String hello(Authentication authentication) {
+        User user = (User) authentication.getPrincipal();
+        return "Welcome "+ user.getName() + " to Secured Endpoint ";
+    }
 }
--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
@@ -3,6 +3,8 @@ package com.sportsmatch.controllers;
 import com.sportsmatch.auth.AuthService;
 import com.sportsmatch.dtos.AuthRequestDTO;
 import com.sportsmatch.services.ValidationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -20,16 +22,11 @@ public class AuthController {
   private final AuthService authService;
   private final ValidationService validationService;
 
-  /**
-   * Registers a user based on the provided credentials.
-   *
-   * @param authRequestDTO The authentication request containing user details.
-   * @param bindingResult The result of the validation process.
-   * @return ResponseEntity indicating the success or failure of the registration. Returns a 400 Bad
-   *     Request with validation errors List&lt;String&gt; if input is invalid. Returns a 200 OK
-   *     if registration is successful.
-   */
   @PostMapping("/register")
+  @Tag(name = "Register")
+  @Operation(
+      summary = "Register new user",
+      description = "Register a new user by providing their email and username.")
   public ResponseEntity<?> register(
       @RequestBody @Valid AuthRequestDTO authRequestDTO, BindingResult bindingResult) {
     if (bindingResult.hasErrors()) {
@@ -39,16 +36,11 @@ public class AuthController {
     return ResponseEntity.ok().build();
   }
 
-  /**
-   * Authenticates a user based on the provided credentials.
-   *
-   * @param authRequestDTO The authentication request containing user credentials.
-   * @param bindingResult The result of the validation process.
-   * @return ResponseEntity indicating the success or failure of the authentication. Returns a 400
-   *     Bad Request with validation errors if input is invalid. Returns a 200 OK with
-   *     authentication details if authentication is successful.
-   */
   @PostMapping("/authenticate")
+  @Tag(name = "Authenticate")
+  @Operation(
+      summary = "Authenticate user",
+      description = "Authenticate a user by providing their email and username.")
   public ResponseEntity<?> authenticate(
       @RequestBody @Valid AuthRequestDTO authRequestDTO, BindingResult bindingResult) {
     if (bindingResult.hasErrors()) {

b1866f9cfece45ac570e63efe705a4611d353e9d
markpernia <perniamanila@gmail.com>
2024-01-29 16:22:08 +0100

    SMA-30: address stylecheck failed test remove unused imports

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/AuthController.java
@@ -7,7 +7,6 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;

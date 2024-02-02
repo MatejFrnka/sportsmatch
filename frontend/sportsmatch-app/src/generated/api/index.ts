e4f3071133e717dcfe039697f9dbfc3d06a45e21
ivamach <ivana.machacikova@gmail.com>
2024-02-02 17:34:55 +0100

    SMA-24: added generated files

---
+++ frontend/sportsmatch-app/src/generated/api/index.ts
@@ -0,0 +1,16 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export { ApiError } from './core/ApiError';
+export { CancelablePromise, CancelError } from './core/CancelablePromise';
+export { OpenAPI } from './core/OpenAPI';
+export type { OpenAPIConfig } from './core/OpenAPI';
+
+export type { AuthRequestDTO } from './models/AuthRequestDTO';
+export type { EventDTO } from './models/EventDTO';
+
+export { EventsControllerService } from './services/EventsControllerService';
+export { ExSecuredEndpointService } from './services/ExSecuredEndpointService';
+export { LoginService } from './services/LoginService';
+export { RegisterService } from './services/RegisterService';
---
+++ frontend/sportsmatch-app/src/generated/api/models/EventDTO.ts
@@ -0,0 +1,17 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type EventDTO = {
+    id?: number;
+    dateStart: string;
+    dateEnd: string;
+    location: string;
+    minElo: number;
+    maxElo: number;
+    title: string;
+    player1Id?: number;
+    player2Id?: number;
+    sport: string;
+};
+

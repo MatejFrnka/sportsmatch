b41d948213debec400868297d306c815cb107648
Kollonay Zoltán <zoltan@kollonay.hu>
2024-02-03 09:59:27 +0100

    feat: created Navbar component

---
+++ frontend/sportsmatch-app/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import { FaMailBulk } from 'react-icons/fa'
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="logo">
+        <FaMailBulk />
+        <span>SportsMingle</span>
+      </div>
+    </nav>
+  )
+}
+export default Navbar

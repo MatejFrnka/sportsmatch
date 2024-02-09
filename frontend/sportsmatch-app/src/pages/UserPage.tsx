2ba652fca66180eb392f0d6686140e90c565a63a
markpernia <perniamanila@gmail.com>
2024-02-09 19:48:24 +0100

    SMA-35: Added Initial HTML structure and CSS style

--- frontend/sportsmatch-app/src/pages/UserPage.tsx
+++ frontend/sportsmatch-app/src/pages/UserPage.tsx
@@ -1,8 +1,47 @@
-
+import '../styles/UserPage.css'
 
 export default function UserPage() {
-    return(
-        <>
-        </>
-    )
+  return (
+    <>
+      <div className="container-sm">
+        <div className="user-page">
+          <div className="row">
+            <hr />
+            <div className="match-wrapper">
+              <div className="col">
+                <p>No upcoming match</p>
+                <a href="#">Find or Host a Match</a>
+              </div>
+            </div>
+          </div>
+          <div className="view-wrapper">
+            <div className="row">
+              <hr />
+              <div className="col">
+                <p>History</p>
+              </div>
+              <div className="col view">
+                <a href="#">View all</a>
+              </div>
+            </div>
+          </div>
+          <div className="history-wrapper">
+            <div className="row">
+              <div className="col">
+                <p>No match history</p>
+              </div>
+            </div>
+          </div>
+          <div className="nearby-wrapper">
+            <p>Nearby</p>
+          </div>
+          <div className="event-wrapper">
+            <div className="row">
+              <div className="col"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }

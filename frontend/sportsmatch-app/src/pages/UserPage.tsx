6606c176efca358aa6bf051b3623104395a0120d
markpernia <perniamanila@gmail.com>
2024-02-10 15:11:33 +0100

    SMA-35: Added match, history and event components to user page

--- frontend/sportsmatch-app/src/pages/UserPage.tsx
+++ frontend/sportsmatch-app/src/pages/UserPage.tsx
@@ -1,22 +1,81 @@
 import '../styles/UserPage.css'
+import Match from '../components/Match'
+import EventHistoryItem from '../components/EventHistoryItem'
+import SportEvent from '../components/SportEvent'
 
 export default function UserPage() {
+  const sampleUserWithEvent = {
+    id: 1,
+    username: `johndoe87`,
+    event: {
+      id: 1,
+      maxElo: 2000,
+      minElo: 1200,
+      dateEnd: '2024-05-02',
+      dateStart: '2024-05-01',
+      location: 'Prague, Stadium A',
+      title: 'Badminton match',
+      sport: 'Badminton',
+      playerOne: 'johndoe87',
+      playerTwo: 'jess_ward',
+    },
+  }
+
+  const sampleUserWithOutEvent = {
+    id: 2,
+    username: `johndoe87`,
+    event: null,
+  }
+
+  const sampleAllEventsDTO = [
+    {
+      id: 1,
+      name: 'Event 1',
+      date: '2024-02-10',
+      location: 'Location 1',
+      userScore: 1,
+      opponentScore: 2,
+    },
+    {
+      id: 2,
+      name: 'Event 1',
+      date: '2024-02-10',
+      location: 'Location 1',
+      userScore: 1,
+      opponentScore: 2,
+    },
+    {
+      id: 3,
+      name: 'Event 1',
+      date: '2024-02-10',
+      location: 'Location 1',
+      userScore: 1,
+      opponentScore: 2,
+    },
+  ]
+
+  const { event } = sampleUserWithEvent
+
   return (
     <>
       <div className="container-sm">
         <div className="user-page">
+          {event === null ? <hr /> : <></>}
           <div className="row">
-            <hr />
-            <div className="match-wrapper">
-              <div className="col">
-                <p>No upcoming match</p>
-                <a href="#">Find or Host a Match</a>
+            {event === null ? (
+              <div className="match-wrapper">
+                <div className="col">
+                  <p>No upcoming match</p>
+                  <a href="#">Find or Host a Match</a>
+                </div>
               </div>
-            </div>
+            ) : (
+              <Match event={event} />
+            )}
           </div>
           <div className="view-wrapper">
+            {event === null ? <hr /> : <></>}
             <div className="row">
-              <hr />
               <div className="col">
                 <p>History</p>
               </div>
@@ -28,18 +87,21 @@ export default function UserPage() {
           <div className="history-wrapper">
             <div className="row">
               <div className="col">
-                <p>No match history</p>
+                {sampleAllEventsDTO === null ? (
+                  <p>No match history</p>
+                ) : (
+                  sampleAllEventsDTO.map((event) => (
+                    <EventHistoryItem key={event.id} eventHistoryDTO={event} />
+                  ))
+                )}
               </div>
             </div>
           </div>
           <div className="nearby-wrapper">
             <p>Nearby</p>
           </div>
-          <div className="event-wrapper">
-            <div className="row">
-              <div className="col"></div>
-            </div>
-          </div>
+          <SportEvent event={sampleUserWithEvent.event} />
+          <SportEvent event={sampleUserWithEvent.event} />
         </div>
       </div>
     </>

0cc4583988b9a07299d331c98a9f62de98fbbecc
JanaDupcakova <jana.dupcakova@gmail.com>
2024-02-13 21:20:53 +0100

    created functions for connecting backend and frontend - not working yet

--- frontend/sportsmatch-app/src/components/LoginComponent.tsx
+++ frontend/sportsmatch-app/src/components/LoginComponent.tsx
@@ -1,19 +1,48 @@
+import { useState, FormEvent } from 'react'
 import '../styles/LoginComponent.css'
 import { FaMailBulk, FaLock, FaGoogle, FaFacebook } from 'react-icons/fa'
+import { LoginService } from '../generated/api'
 
 function LoginComponent() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+
+    try {
+      const response = await LoginService.login({ email: email, password: password })
+
+      console.log(response.data)
+    } catch (error) {
+      console.error('The username or password is invalid', error)
+    }
+  }
+
   return (
     <div className="wrapper">
-      <form action="">
+      <form onSubmit={handleSubmit}>
         <h1>Log in</h1>
         <div className="input-box">
           <label htmlFor="email"></label>
-          <input type="email" placeholder="E-mail addres" required />
+          <input
+            type="email"
+            placeholder="E-mail addres"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
           <FaMailBulk className="icon" />
         </div>
         <div className="input-box">
           <label htmlFor="password"></label>
-          <input type="password" placeholder="password" required />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
           <FaLock className="icon" />
         </div>
         <div className="remember-forgot">
@@ -43,6 +72,21 @@ function LoginComponent() {
       </form>
     </div>
   )
+}
+
+async function sendForm(
+  e: FormEvent<HTMLFormElement>,
+  email: string,
+  password: string,
+) {
+  e.preventDefault()
+
+  try {
+    const response = await LoginService.login({ email: email, password: password })
+    console.log(response.data)
+  } catch (error) {
+    console.error('The username or password is invalid', error)
+  }
 }
 
 export default LoginComponent
--- frontend/sportsmatch-app/src/pages/Login.tsx
+++ frontend/sportsmatch-app/src/pages/Login.tsx
@@ -1,4 +1,4 @@
-import LoginComponent from '../components/LoginComponent'
+import LoginComponent from '../components/LoginComponent.tsx'
 
 export default function Login() {
   return (

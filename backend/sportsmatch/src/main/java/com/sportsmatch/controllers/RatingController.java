c4da98182adb9c3dcd800ded5c448c92d4bf9eb5
markpernia <perniamanila@gmail.com>
2024-02-14 13:02:30 +0100

    SMA-45: added add rating endpoint

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/RatingController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/RatingController.java
@@ -1,19 +1,39 @@
 package com.sportsmatch.controllers;
 
 import com.sportsmatch.dtos.RatingDTO;
+import com.sportsmatch.services.RatingService;
+import com.sportsmatch.services.ValidationService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/api/v1/rating")
 public class RatingController {
 
-    @PostMapping("/add")
-    public String addRating(@RequestBody @Valid RatingDTO ratingDTO) {
+    private final RatingService ratingService;
+    private final ValidationService validationService;
 
-        return "";
+    @PostMapping("/add")
+    public ResponseEntity<?> addRating(@RequestBody @Valid RatingDTO ratingDTO,
+                            Authentication authentication,
+                            BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return ResponseEntity.badRequest().body(validationService.getAllErrors(bindingResult));
+        }
+        try{
+            ratingService.addRating(ratingDTO,authentication);
+            return ResponseEntity.ok().build();
+        } catch (ResponseStatusException e) {
+            return ResponseEntity.status(e.getStatusCode()).build();
+        }
     }
 }

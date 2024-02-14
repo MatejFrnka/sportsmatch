d52decb71ced856eee02767ce91320f020286d20
markpernia <perniamanila@gmail.com>
2024-02-14 12:57:57 +0100

    SMA-45: added findEventPlayer by event

--- backend/sportsmatch/src/main/java/com/sportsmatch/repositories/EventPlayerRepository.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/repositories/EventPlayerRepository.java
@@ -1,14 +1,19 @@
 package com.sportsmatch.repositories;
 
+import com.sportsmatch.models.Event;
 import com.sportsmatch.models.EventPlayer;
 import com.sportsmatch.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface EventPlayerRepository extends JpaRepository<EventPlayer, Long> {
 
   EventPlayer findEventPlayerById(Long id);
 
-  EventPlayer findEventPlayerByPlayer(User player);
+  Optional<EventPlayer> findEventPlayerByPlayer(User player);
+  List<EventPlayer> findEventPlayersByEvent(Event event);
 }

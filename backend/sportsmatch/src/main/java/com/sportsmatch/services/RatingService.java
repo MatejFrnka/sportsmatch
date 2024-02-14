912deec3d40542d694260c67b45ab16f31fe9125
markpernia <perniamanila@gmail.com>
2024-02-14 11:16:31 +0100

    SMA-45: Implement addRating

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/services/RatingService.java
@@ -0,0 +1,62 @@
+package com.sportsmatch.services;
+
+import com.sportsmatch.dtos.RatingDTO;
+import com.sportsmatch.mappers.RatingMapper;
+import com.sportsmatch.models.*;
+import com.sportsmatch.repositories.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class RatingService {
+
+  private final RatingRepository ratingRepository;
+  private final UserEventRatingRepository userEventRatingRepository;
+  private final EventPlayerRepository eventPlayerRepository;
+  private final RatingMapper ratingMapper;
+
+  public void addRating(RatingDTO ratingDTO, Authentication authentication) {
+    User player = (User) authentication.getPrincipal();
+    EventPlayer eventPlayer = getEventPlayer(player);
+    User opponent = findOpponent(eventPlayer, player);
+    Rating userRating = ratingMapper.toUserRatingEntity(ratingDTO);
+    Rating eventRating = ratingMapper.toEventRatingEntity(ratingDTO);
+
+    eventPlayer.setMyScore(ratingDTO.getMyScore());
+    eventPlayer.setOpponentScore(ratingDTO.getOpponentScore());
+
+    UserEventRating userEventRating = UserEventRating.builder()
+            .userRating(userRating)
+            .eventRating(eventRating)
+            .player(player)
+            .opponent(opponent)
+            .event(eventPlayer.getEvent())
+            .build();
+    ratingRepository.save(userRating);
+    ratingRepository.save(eventRating);
+    userEventRatingRepository.save(userEventRating);
+  }
+
+  private EventPlayer getEventPlayer(User player) {
+    Optional<EventPlayer> eventPlayerOptional =
+        eventPlayerRepository.findEventPlayerByPlayer(player);
+      return eventPlayerOptional.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
+
+  private User findOpponent(EventPlayer eventPlayer, User player) {
+    List<EventPlayer> eventPlayers =
+        eventPlayerRepository.findEventPlayersByEvent(eventPlayer.getEvent());
+    return eventPlayers.stream()
+        .map(EventPlayer::getPlayer)
+        .filter(p -> !p.getId().equals(player.getId()))
+        .findFirst()
+        .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+  }
+}

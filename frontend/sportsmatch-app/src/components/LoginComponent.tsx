1f8725166405d3a09d64f80dcf968b1d04c7e5b7
JanaDupcakova <jana.dupcakova@gmail.com>
2024-02-15 21:58:45 +0100

    added error message for login incorrect input

--- frontend/sportsmatch-app/src/components/LoginComponent.tsx
+++ frontend/sportsmatch-app/src/components/LoginComponent.tsx
@@ -1,22 +1,27 @@
 import { useState, FormEvent } from 'react'
 import '../styles/LoginComponent.css'
 import { FaMailBulk, FaLock, FaGoogle, FaFacebook } from 'react-icons/fa'
-import {LoginService, OpenAPI} from '../generated/api'
+import { LoginService, OpenAPI } from '../generated/api'
 
 function LoginComponent() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [errorMessage, setErrorMessage] = useState('')
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
 
     try {
-      const response = await LoginService.login({ email: email, password: password })
+      const response = await LoginService.login({
+        email: email,
+        password: password,
+      })
 
       console.log(response.data)
       OpenAPI.TOKEN = response.token // this line tells OpenAPI to authenticate with this header
     } catch (error) {
-      console.error('The username or password is invalid', error)
+      console.error('Login Error', error)
+      setErrorMessage('The email address or password is invalid.')
     }
   }
 
@@ -24,11 +29,12 @@ function LoginComponent() {
     <div className="wrapper">
       <form onSubmit={handleSubmit}>
         <h1>Log in</h1>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
         <div className="input-box">
           <label htmlFor="email"></label>
           <input
             type="email"
-            placeholder="E-mail addres"
+            placeholder="E-mail address"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
             required
@@ -83,7 +89,10 @@ async function sendForm(
   e.preventDefault()
 
   try {
-    const response = await LoginService.login({ email: email, password: password })
+    const response = await LoginService.login({
+      email: email,
+      password: password,
+    })
     console.log(response.data)
   } catch (error) {
     console.error('The username or password is invalid', error)

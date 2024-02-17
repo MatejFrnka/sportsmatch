2b5e811c20830657722cca8355bcc6ed590147a6
markpernia <perniamanila@gmail.com>
2024-02-17 10:22:03 +0100

    SMA-45: copy user service from SMA-43

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/services/UserServiceImp.java
@@ -0,0 +1,34 @@
+package com.sportsmatch.services;
+
+
+import com.sportsmatch.models.User;
+import com.sportsmatch.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImp implements UserService {
+
+  private final UserRepository userRepository;
+
+  /**
+   * This method returns the user from the securityContextHolder
+   * @return an authenticated user
+   */
+  @Override
+  public User getUserFromTheSecurityContextHolder() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication != null && authentication.isAuthenticated() && authentication.getPrincipal() instanceof UserDetails userDetails) {
+      return userRepository.findByEmail(userDetails.getUsername()).orElseThrow();
+    } else {
+      throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not authenticated");
+    }
+  }
+
+}

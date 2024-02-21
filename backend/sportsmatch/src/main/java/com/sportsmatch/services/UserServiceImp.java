07f3a4f047e8052365a5a8c376ffbd3cf3c6cf40
markpernia <perniamanila@gmail.com>
2024-02-21 16:28:26 +0100

    SMA-47: Implement updateUserInfo

--- backend/sportsmatch/src/main/java/com/sportsmatch/services/UserServiceImp.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/services/UserServiceImp.java
@@ -1,7 +1,15 @@
 package com.sportsmatch.services;
 
+import com.sportsmatch.dtos.SportDTO;
+import com.sportsmatch.dtos.UserInfoDTO;
+import com.sportsmatch.mappers.SportMapper;
+import com.sportsmatch.models.Gender;
+import com.sportsmatch.models.SportUser;
 import com.sportsmatch.models.User;
+import com.sportsmatch.repositories.SportRepository;
+import com.sportsmatch.repositories.SportUserRepository;
 import com.sportsmatch.repositories.UserRepository;
+import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -10,11 +18,16 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 @Service
 @RequiredArgsConstructor
 public class UserServiceImp implements UserService {
 
   private final UserRepository userRepository;
+  private final SportMapper sportMapper;
+  private final SportRepository sportRepository;
 
   /**
    * Retrieves the currently authenticated user from the security context.
@@ -30,6 +43,27 @@ public class UserServiceImp implements UserService {
       return userRepository.findByEmail(userDetails.getUsername()).orElseThrow();
     } else {
       throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "User not authenticated");
+    }
+  }
+
+  @Transactional
+  public void updateUserInfo(UserInfoDTO userInfoDTO) {
+    User user = getUserFromTheSecurityContextHolder();
+    user.setName(userInfoDTO.getUserName());
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    user.setDateOfBirth(LocalDate.parse(userInfoDTO.getDateOfBirth(), formatter));
+    user.setGender(Gender.valueOf(userInfoDTO.getGender().toUpperCase()));
+    linkUserWithSport(userInfoDTO, user);
+    userRepository.save(user);
+  }
+
+  private void linkUserWithSport(UserInfoDTO userInfoDTO, User user) {
+    for (SportDTO sportDTO : userInfoDTO.getSports()) {
+      if (sportRepository.findSportByName(sportDTO.getName()).isEmpty()) {
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Sport '" + sportDTO.getName() + "' not found.");
+      }
+      SportUser sportUser = new SportUser(user, sportMapper.toEntity(sportDTO));
+      user.getSportUsers().add(sportUser);
     }
   }
 }

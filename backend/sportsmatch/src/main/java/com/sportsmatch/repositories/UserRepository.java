bee39acb5a1a481b39e24477592d3f0ac5acb15d
Kollonay Zoltán <zoltan@kollonay.hu>
2024-02-22 14:16:25 +0100

    Feat: Task done

--- backend/sportsmatch/src/main/java/com/sportsmatch/repositories/UserRepository.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/repositories/UserRepository.java
@@ -5,12 +5,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+  @Transactional
   Optional<User> findUserById(Long id);
 
   Optional<User> findByEmail(String email);

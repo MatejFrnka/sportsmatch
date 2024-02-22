097edddcf890fedcadd1f7203fe9e5b0a344caa7
Kollonay Zoltán <zoltan@kollonay.hu>
2024-02-22 20:03:42 +0100

    Feat: modified test

--- backend/sportsmatch/src/test/java/com/sportsmatch/services/EventServiceTest.java
+++ backend/sportsmatch/src/test/java/com/sportsmatch/services/EventServiceTest.java
@@ -14,22 +14,17 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class EventServiceTest {
-
   private UserService userService;
   private EventService eventService;
-
   private User loggedUser;
   private User otherUser;
-  private EventPlayer loggedEventPlayer;
-  private EventPlayer otherEventPlayer;
+
   @BeforeEach
   void setUp() {
     userService = mock(UserService.class);
     eventService = new EventService(userService);
     loggedUser = createUser("loggedUser");
     otherUser = createUser("otherUser");
-    loggedEventPlayer = createEventPlayer(loggedUser, 2, 2);
-    otherEventPlayer = createEventPlayer(otherUser, 2, 2);
   }
 
   // Create an user
@@ -47,10 +42,16 @@ class EventServiceTest {
     eventPlayer.setOpponentScore(opponentScore);
     return eventPlayer;
   }
+
   @Test
   void checkScoreMatchExpectStatus_MATCH() {
     // ARRANGE:
 
+    // Event players
+    EventPlayer loggedEventPlayer = createEventPlayer(loggedUser, 2, 2);
+    EventPlayer otherEventPlayer = createEventPlayer(otherUser, 2, 2);
+
+    // Set of the eventPlayers
     Set<EventPlayer> eventPlayers = new HashSet<>();
     eventPlayers.add(loggedEventPlayer);
     eventPlayers.add(otherEventPlayer);
@@ -68,10 +69,6 @@ class EventServiceTest {
   @Test
   void checkScoreMatchExpectStatus_MISMATCH() {
     // ARRANGE:
-
-    // Users
-    User loggedUser = createUser("loggedUser");
-    User otherUser = createUser("otherUser");
 
     // Event players
     EventPlayer loggedEventPlayer = createEventPlayer(loggedUser, 3, 2);
@@ -95,25 +92,9 @@ class EventServiceTest {
   void checkScoreMatchExpectStatus_WAITING_FOR_RATING() {
     // ARRANGE:
 
-    // Logged user
-    User loggedUser = new User();
-    loggedUser.setName("loggedUser");
-
-    // Other user
-    User otherUser = new User();
-    otherUser.setName("otherUser");
-
-    // Logged event player
-    EventPlayer loggedEventPlayer = new EventPlayer();
-    loggedEventPlayer.setPlayer(loggedUser);
-    loggedEventPlayer.setMyScore(3);
-    loggedEventPlayer.setOpponentScore(2);
-
-    // other event player
-    EventPlayer otherEventPlayer = new EventPlayer();
-    otherEventPlayer.setPlayer(otherUser);
-    otherEventPlayer.setMyScore(null);
-    otherEventPlayer.setOpponentScore(null);
+    // Event players
+    EventPlayer loggedEventPlayer = createEventPlayer(loggedUser, 3, 2);
+    EventPlayer otherEventPlayer = createEventPlayer(otherUser, null, null);
 
     // Set of the eventPlayers
     Set<EventPlayer> eventPlayers = new HashSet<>();
@@ -133,15 +114,8 @@ class EventServiceTest {
   void checkScoreMatchExpectStatus_INVALID_PLAYER() {
     // ARRANGE:
 
-    // Logged user
-    User loggedUser = new User();
-    loggedUser.setName("loggedUser");
-
-    // Logged event player
-    EventPlayer loggedEventPlayer = new EventPlayer();
-    loggedEventPlayer.setPlayer(loggedUser);
-    loggedEventPlayer.setMyScore(3);
-    loggedEventPlayer.setOpponentScore(2);
+    // Event players
+    EventPlayer loggedEventPlayer = createEventPlayer(loggedUser, 3, 2);
 
     // Set of the eventPlayers
     Set<EventPlayer> eventPlayers = new HashSet<>();
@@ -155,5 +129,4 @@ class EventServiceTest {
     // Assert:
     assertEquals(EventStatusOptions.INVALID_PLAYER, result);
   }
-
 }

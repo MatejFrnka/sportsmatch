82883a11d1a281e0c7d027414af75b2e0f5fc746
ivamach <ivana.machacikova@gmail.com>
2024-03-08 21:40:29 +0100

    SMA-56: jwt token is now stored in localStorage, added redirection to url that was entered before login

--- frontend/sportsmatch-app/src/components/LoginComponent.tsx
+++ frontend/sportsmatch-app/src/components/LoginComponent.tsx
@@ -2,13 +2,15 @@ import { useState, FormEvent } from 'react'
 import '../styles/LoginComponent.css'
 import { FaMailBulk, FaLock } from 'react-icons/fa'
 import { LoginService, OpenAPI } from '../generated/api'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 function LoginComponent() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const navigate = useNavigate()
+
+  const location = useLocation()
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -21,7 +23,12 @@ function LoginComponent() {
 
       console.log(response.data)
       OpenAPI.TOKEN = response.token // this line tells OpenAPI to authenticate with this header
-      navigate('/')
+      localStorage.setItem('token', response.token)
+      if (location.state != null) {
+        navigate(location.state)
+      } else {
+        navigate('/')
+      }
     } catch (error) {
       console.error('Login Error', error)
       setErrorMessage('The email address or password is invalid.')

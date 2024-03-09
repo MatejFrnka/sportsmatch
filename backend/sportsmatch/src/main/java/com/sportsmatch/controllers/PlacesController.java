19833f97f6fa7bdb0ac71cf4a326a7152b2f8b30
Kollonay Zoltán <zoltan@kollonay.hu>
2024-03-09 17:30:48 +0100

    Feat: added Controller and Service class

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/PlacesController.java
@@ -0,0 +1,11 @@
+package com.sportsmatch.controllers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/places")
+public class PlacesController {
+}
---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/services/PlaceService.java
@@ -0,0 +1,9 @@
+package com.sportsmatch.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class PlaceService {
+}

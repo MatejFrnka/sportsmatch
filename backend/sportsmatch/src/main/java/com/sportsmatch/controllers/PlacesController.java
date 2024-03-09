5f94b18139ae107022c3590b6e86ddf6da7bcaf5
Kollonay Zoltán <zoltan@kollonay.hu>
2024-03-09 20:23:17 +0100

    Feat: added addNewPlace method to controller

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/PlacesController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/PlacesController.java
@@ -1,6 +1,13 @@
 package com.sportsmatch.controllers;
 
+import com.sportsmatch.dtos.PlaceDTO;
+import com.sportsmatch.models.Place;
+import com.sportsmatch.services.PlaceService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/places")
 public class PlacesController {
+
+  private final PlaceService placeService;
+
+  @PostMapping("/add")
+  public ResponseEntity<Place> addNewPlace(@RequestBody PlaceDTO placeDTO) {
+    return ResponseEntity.status(HttpStatus.CREATED).body(placeService.addNewPlace());
+  }
 }
--- backend/sportsmatch/src/main/java/com/sportsmatch/services/PlaceService.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/services/PlaceService.java
@@ -1,9 +1,14 @@
 package com.sportsmatch.services;
 
+import com.sportsmatch.models.Place;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class PlaceService {
+
+  public Place addNewPlace(){
+    return null;
+  }
 }

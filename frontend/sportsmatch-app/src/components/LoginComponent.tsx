151e24fbe6e22ad350cc1014b923a6fcccfd1be9
Ivana Machacikova <131284389+ivamach@users.noreply.github.com>
2024-03-11 18:22:40 +0100

    SMA-56: handle logic that redirects the user to login page if the user is not logged in (#47)

--- frontend/sportsmatch-app/src/components/LoginComponent.tsx
+++ frontend/sportsmatch-app/src/components/LoginComponent.tsx
@@ -1,14 +1,16 @@
 import { useState, FormEvent } from 'react'
 import '../styles/LoginComponent.css'
 import { FaMailBulk, FaLock } from 'react-icons/fa'
-import { LoginService, OpenAPI } from '../generated/api'
-import { Link, useNavigate } from 'react-router-dom'
+import { LoginService } from '../generated/api'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
 
 function LoginComponent() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [errorMessage, setErrorMessage] = useState('')
   const navigate = useNavigate()
+
+  const location = useLocation()
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault()
@@ -20,8 +22,12 @@ function LoginComponent() {
       })
 
       console.log(response.data)
-      OpenAPI.TOKEN = response.token // this line tells OpenAPI to authenticate with this header
-      navigate('/')
+      localStorage.setItem('token', response.token)
+      if (location.state != null) {
+        navigate(location.state)
+      } else {
+        navigate('/')
+      }
     } catch (error) {
       console.error('Login Error', error)
       setErrorMessage('The email address or password is invalid.')
---
+++ frontend/sportsmatch-app/src/generated/api/models/UserInfoDTO.ts
@@ -0,0 +1,12 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+import type { SportDTO } from './SportDTO';
+export type UserInfoDTO = {
+    userName: string;
+    dateOfBirth: string;
+    gender: string;
+    sports?: Array<SportDTO>;
+};
+

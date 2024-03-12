05ba5ac2c7414a88edf34eb6c4d17a4c10bdb44a
Kollonay Zoltán <zoltan@kollonay.hu>
2024-03-12 12:16:01 +0100

    Feat: added searchPlaceByName method test in PlaceServiceTest

--- backend/sportsmatch/src/test/java/com/sportsmatch/services/PlaceServiceTest.java
+++ backend/sportsmatch/src/test/java/com/sportsmatch/services/PlaceServiceTest.java
@@ -13,7 +13,12 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.*;
 
 class PlaceServiceTest {
@@ -35,7 +40,7 @@ class PlaceServiceTest {
   private PlaceDTO createTestPlaceDTO() {
     return PlaceDTO.builder()
         .name("Place name")
-        .address("Budapest")
+        .address("Place address")
         .latitude(40.6892F)
         .longitude(74.0445F)
         .build();
@@ -45,7 +50,7 @@ class PlaceServiceTest {
   private Place createTestPlace() {
     return Place.builder()
         .name("Place name")
-        .address("Budapest")
+        .address("Place address")
         .latitude(40.6892F)
         .longitude(74.0445F)
         .build();
@@ -87,5 +92,46 @@ class PlaceServiceTest {
 
     // Verifying the placeRepository.save() method was called only 1 time.
     verify(placeRepository, times(0)).save(any());
+  }
+
+  @Test
+  void searchPlacesByNameShouldReturnCorrectDTOs() {
+    // Name to search for
+    String name = "Place name";
+    List<Place> places = Arrays.asList(
+        Place.builder().id(1L).name("Place name").build(),
+        Place.builder().id(2L).name("Place name").build());
+
+    // Map the list of Place entity to a list of PlaceDTO objects
+    List<PlaceDTO> expectedDTOs = places.stream()
+        .map(placeMapper::toDTO)
+        .toList();
+
+    // Mock the behavior of PlaceRepository.findByName(name)to return the given list of Places
+    when(placeRepository.findByName(name)).thenReturn(places);
+
+    // Call the searchPlacesByName method with the provided name
+    List<PlaceDTO> resultDTOs = placeService.searchPlacesByName(name);
+
+    // Assert the size of the excepted and result lists are equal
+    // Iterate over pair of excepted and result DTOs to ensure they match
+    assertEquals(expectedDTOs.size(), resultDTOs.size());
+    for (int i = 0; i < expectedDTOs.size(); i++) {
+      assertEquals(expectedDTOs.get(i), resultDTOs.get(i));
+    }
+  }
+  @Test
+  void searchPlacesByNameShouldReturnEmptyListWhenNoPlacesFound() {
+    // Name to search for
+    String name = "Non-exist place name";
+
+    // Mock the behavior of PlaceRepository.findByName(name)to return empty list of Places
+    when(placeRepository.findByName(name)).thenReturn(Collections.emptyList());
+
+    // Call the searchPlacesByName method with the provided name
+    List<PlaceDTO> resultDTOs = placeService.searchPlacesByName(name);
+
+    // Assert the list is empty with the given name
+    assertTrue(resultDTOs.isEmpty());
   }
 }

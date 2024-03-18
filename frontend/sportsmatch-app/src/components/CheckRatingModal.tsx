c82823f38824e66201201b28b7487d43f48d5e7b
ivamach <ivana.machacikova@gmail.com>
2024-03-18 18:21:17 +0100

    SMA-61: created CheckRatingModal component

---
+++ frontend/sportsmatch-app/src/components/CheckRatingModal.tsx
@@ -0,0 +1,31 @@
+import { useEffect } from 'react'
+import Modal from '../components/Modal'
+import RateGameComponent from '../components/RateGameComponent'
+import { RatingControllerService, ApiError, OpenAPI } from '../generated/api'
+import useModal from '../hooks/UseModal'
+
+export default function CheckRatingModal() {
+  const { isOpen, toggle } = useModal()
+  useEffect(() => {
+    const init = async () => {
+      try {
+        OpenAPI.TOKEN = localStorage.getItem('token')!
+        const response = await RatingControllerService.checkRating()
+        if (response[0] && !isOpen) {
+          toggle()
+        }
+      } catch (error) {
+        console.error(error as ApiError)
+      }
+    }
+    init()
+  })
+
+  return (
+    <>
+      <Modal isOPen={isOpen} toggle={toggle} preventClosing={true}>
+        <RateGameComponent toggle={toggle} />
+      </Modal>
+    </>
+  )
+}

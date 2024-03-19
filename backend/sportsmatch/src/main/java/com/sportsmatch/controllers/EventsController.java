5a8e73d8f86f0dd1d74cf3d25783d252453d344e
markpernia <perniamanila@gmail.com>
2024-03-19 22:30:25 +0100

    SMA-64: added mock nearby endpoint

--- backend/sportsmatch/src/main/java/com/sportsmatch/controllers/EventsController.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/controllers/EventsController.java
@@ -2,6 +2,7 @@ package com.sportsmatch.controllers;
 
 import com.sportsmatch.dtos.EventDTO;
 import com.sportsmatch.dtos.EventHistoryDTO;
+import com.sportsmatch.dtos.RequestEventDTO;
 import com.sportsmatch.models.Event;
 import com.sportsmatch.services.EventService;
 import jakarta.validation.Valid;
@@ -48,7 +49,6 @@ public class EventsController {
     return ResponseEntity.ok().body(listOfEvents);
   }
 
-
   /**
    * This endpoint returns the history of the finished events by the logged-in user.
    *
@@ -58,5 +58,11 @@ public class EventsController {
   @GetMapping("/event-history")
   public List<EventHistoryDTO> getEventsHistory(final Pageable pageable) {
     return eventService.getEventsHistory(pageable);
+  }
+
+  @PostMapping("/nearby")
+  public ResponseEntity<?> getNearbyEvents(@RequestBody RequestEventDTO requestEventDTO) {
+    // mock endpoint currently assumes no sports are supplied returning all events
+    return ResponseEntity.ok().body(eventService.FilterEvent(requestEventDTO));
   }
 }

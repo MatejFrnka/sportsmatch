920355ee1d2b777b3e374445e95d3949fbf3a669
markpernia <perniamanila@gmail.com>
2024-03-19 22:29:25 +0100

    SMA-64: created RequestEventDTO

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/dtos/RequestEventDTO.java
@@ -0,0 +1,21 @@
+package com.sportsmatch.dtos;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class RequestEventDTO {
+
+    private List<String> sportsName;
+    private double longitude;
+    private double latitude;
+}

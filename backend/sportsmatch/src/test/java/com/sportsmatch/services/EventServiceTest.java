22098b1a2585f22e4c4dd98930c2d9489e0ab3e1
ivamach <ivana.machacikova@gmail.com>
2024-03-21 12:31:51 +0100

    SMA-72: added tests for joinEvent method

--- backend/sportsmatch/src/test/java/com/sportsmatch/services/EventServiceTest.java
+++ backend/sportsmatch/src/test/java/com/sportsmatch/services/EventServiceTest.java
@@ -1,9 +1,12 @@
 package com.sportsmatch.services;
 
 import com.sportsmatch.BaseTest;
+import com.sportsmatch.models.Event;
 import com.sportsmatch.models.EventPlayer;
 import com.sportsmatch.models.EventStatusOptions;
 import com.sportsmatch.models.User;
+import com.sportsmatch.repositories.EventPlayerRepository;
+import com.sportsmatch.repositories.EventRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -12,13 +15,18 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class EventServiceTest extends BaseTest {
+  @Mock private EventRepository eventRepository;
+  @Mock private EventPlayerRepository eventPlayerRepository;
   @Mock private UserService userService;
   @InjectMocks private EventService eventService;
   private User loggedUser;
@@ -134,5 +142,76 @@ class EventServiceTest extends BaseTest {
 
     // Assert:
     assertEquals(EventStatusOptions.INVALID_PLAYER, result);
+  }
+
+  @Test
+  void joinEventAddsUserToEvent() {
+    // Arrange:
+    // User:
+    when(userService.getUserFromContext()).thenReturn(loggedUser);
+
+    // Event
+    Event event = new Event();
+    event.setId(1L);
+    when(eventRepository.findEventById(1L)).thenReturn(Optional.of(event));
+
+    // EventPlayer
+    when(eventPlayerRepository.findEventPlayerByEventAndPlayer(event, loggedUser))
+        .thenReturn(Optional.empty());
+    EventPlayer eventPlayer = new EventPlayer();
+    eventPlayer.setEvent(event);
+    eventPlayer.setPlayer(loggedUser);
+
+    // Act:
+    try {
+      eventService.joinEvent(1L);
+    } catch (Exception e) {
+      throw new RuntimeException(e);
+    }
+
+    // Assert
+    verify(eventPlayerRepository, times(1)).save(any(EventPlayer.class));
+  }
+  @Test
+  void joinEventThrowExceptionWhenEventIsFull() {
+    // Arrange:
+    // User:
+    when(userService.getUserFromContext()).thenReturn(loggedUser);
+
+    // Event
+    Event event = new Event();
+    event.setId(1L);
+    when(eventRepository.findEventById(1L)).thenReturn(Optional.of(event));
+
+    // Add players to Event
+    EventPlayer player1 = new EventPlayer();
+    EventPlayer player2 = new EventPlayer();
+    event.getPlayers().add(player1);
+    event.getPlayers().add(player2);
+
+    // Assert:
+    assertThrows(Exception.class, () -> eventService.joinEvent(1L));
+  }
+
+  @Test
+  void joinEventThrowsExceptionWhenUserHasAlreadyJoined() {
+    // Arrange:
+    // User:
+    when(userService.getUserFromContext()).thenReturn(loggedUser);
+
+    // Event
+    Event event = new Event();
+    event.setId(1L);
+    when(eventRepository.findEventById(1L)).thenReturn(Optional.of(event));
+
+    // EventPlayer
+    EventPlayer eventPlayer = new EventPlayer();
+    eventPlayer.setEvent(event);
+    eventPlayer.setPlayer(loggedUser);
+    when(eventPlayerRepository.findEventPlayerByEventAndPlayer(event, loggedUser))
+            .thenReturn(Optional.of(eventPlayer));
+
+    // Assert:
+    assertThrows(Exception.class, () -> eventService.joinEvent(1L));
   }
 }

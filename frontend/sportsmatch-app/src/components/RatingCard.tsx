68ef032ec8f5240b82ce5224b44f4a87fe9f1c21
markpernia <perniamanila@gmail.com>
2024-03-28 09:33:18 +0100

    SMA-73: Created RatingCard component

---
+++ frontend/sportsmatch-app/src/components/RatingCard.tsx
@@ -0,0 +1,22 @@
+import '../styles/RatingCard.css'
+
+export default function RatingCard() {
+  return (
+    <>
+      <div className="rated-wrapper">
+        <div className="row rated">
+          <div className="col-2">
+            <img className="rated-img" src="" alt="" />
+          </div>
+          <div className="col">
+            <h5>Name</h5>
+            <p>
+              Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam
+              erat volutpat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}

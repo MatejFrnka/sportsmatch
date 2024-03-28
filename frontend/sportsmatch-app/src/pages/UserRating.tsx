bb309f747e8e176baea1dd9dc6d174231c02a1b9
markpernia <perniamanila@gmail.com>
2024-03-28 19:00:55 +0100

    SMA-73: simulate data response from api

--- frontend/sportsmatch-app/src/pages/UserRating.tsx
+++ frontend/sportsmatch-app/src/pages/UserRating.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import RatingCard from '../components/RatingCard'
 import UserCard from '../components/UserCard'
 import { RatingDTO, UserDTO } from '../generated/api'
@@ -17,18 +18,87 @@ const sampleUser: UserDTO = {
   elo: 887,
 }
 
-const sampleRated: RatingDTO = {
-  userTextRating: `Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam
-  erat volutpat.`,
-  userStarRating: 4,
-  eventStarRating: 5,
-  myScore: 10,
-  opponentScore: 5,
-}
+const sampleData : RatingDTO[] = [
+  {
+    name: 'Alex S.',
+    userTextRating: "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aliquam erat volutpat.",
+    userStarRating: 4,
+    eventStarRating: 5,
+    myScore: 10,
+    opponentScore: 5,
+  },
+  {
+    name: 'John D.',
+    userTextRating: "Suspendisse potenti. Nullam porta enim eu tortor. Sed ante.",
+    userStarRating: 3,
+    eventStarRating: 4,
+    myScore: 8,
+    opponentScore: 6,
+  },
+  {
+    name: 'Bella S.',
+    userTextRating: "Fusce ultrices fringilla metus. Nam commodo suscipit quam.",
+    userStarRating: 5,
+    eventStarRating: 4,
+    myScore: 12,
+    opponentScore: 4,
+  },
+  {
+    name: 'Robert D.',
+    userTextRating: "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed aliquam libero vitae velit.",
+    userStarRating: 2,
+    eventStarRating: 3,
+    myScore: 7,
+    opponentScore: 8,
+  },
+  {
+    name: 'Anna A.',
+    userTextRating: "Quisque egestas diam in arcu. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus.",
+    userStarRating: 4,
+    eventStarRating: 5,
+    myScore: 9,
+    opponentScore: 5,
+  }
+];
 
-const ratedName = `Alex S`
 
 export default function UserRating() {
+
+  const [ ratings, setRatings ] = useState<RatingDTO[]>([])
+  const [ currentPage, setCurrentPage] = useState<number>(1)
+  const itemsPerPage = 3; //simulate pagination
+
+  const loadMore = () => {
+    const nextPage = currentPage + 1;
+    const startIndex = (nextPage - 1) * itemsPerPage;
+    const endIndex = nextPage * itemsPerPage;
+    const newRatings = sampleData.slice(startIndex, endIndex);
+    
+    if (newRatings.length > 0) {
+      setRatings(prevRatings => [...prevRatings, ...newRatings]);
+      setCurrentPage(nextPage);
+    } else {
+      // Handle no more data to load
+      console.log("No more data to load");
+    }
+  };
+
+  useEffect(() => {
+    const startIndex = (currentPage - 1) * itemsPerPage;
+    const endIndex = currentPage * itemsPerPage;
+    const initialData = sampleData.slice(startIndex, endIndex);
+    setRatings(initialData);
+  }, []); 
+
+  /*
+
+  useEffect(() => {
+    const response = async () => {
+      side effect implementation here
+    }, [dependency here]);
+
+  */
+
   return (
     <>
       <div className="row">
@@ -40,12 +110,14 @@ export default function UserRating() {
         <div className="col">
           <div className="row">
             <div className="col">
-              <RatingCard rating={sampleRated} name={ratedName}/>
+              {ratings.map((rating, index) =>(
+                <RatingCard rating={rating} key={index}/>
+              ))}
             </div>
           </div>
           <div className="row load-btn">
             <div className="col">
-              <button>Load More</button>
+              <button onClick={loadMore}>Load More</button>
             </div>
           </div>
         </div>

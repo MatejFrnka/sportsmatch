f5feae59ae1c1430cccd9a7915c3eb7c4ea4dc19
ivamach <ivana.machacikova@gmail.com>
2024-03-29 20:33:10 +0100

    SMA-68: created custom queries for star rating counts and average rating

--- backend/sportsmatch/src/main/java/com/sportsmatch/repositories/UserEventRatingRepository.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/repositories/UserEventRatingRepository.java
@@ -3,12 +3,26 @@ package com.sportsmatch.repositories;
 import com.sportsmatch.models.Event;
 import com.sportsmatch.models.User;
 import com.sportsmatch.models.UserEventRating;
+import com.sportsmatch.dtos.UserStarRatingCountDTO;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface UserEventRatingRepository extends JpaRepository<UserEventRating, Long> {
   Optional<UserEventRating> findUserEventRatingByEventAndPlayer(Event event, User player);
+
+  @Query(
+      "SELECT new com.sportsmatch.dtos.UserStarRatingCountDTO(r.starRating, COUNT(r.starRating))"
+          + " FROM UserEventRating uer JOIN Rating r ON uer.userRating.id = r.id WHERE uer.opponent.id = :id"
+          + " GROUP BY r.starRating")
+  List<UserStarRatingCountDTO> findRatingsCount(@Param("id") Long id);
+
+  @Query(
+      "SELECT AVG(r.starRating) FROM UserEventRating uer JOIN Rating r ON uer.userRating.id = r.id WHERE uer.opponent.id = :id")
+  Double findAverageRating(@Param("id") Long id);
 }

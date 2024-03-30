eb1402522d085fa08f39f81e511e11d254e98f10
ivamach <ivana.machacikova@gmail.com>
2024-03-30 12:06:00 +0100

    SMA-68: added createdAt timestamp to Rating

--- backend/sportsmatch/src/main/java/com/sportsmatch/models/Rating.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/models/Rating.java
@@ -2,7 +2,10 @@ package com.sportsmatch.models;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -17,10 +20,16 @@ public class Rating {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
+
   @Column(name = "text_rating")
   private String textRating;
+
   @Column(name = "star_rating")
   private Integer starRating;
+
+  @Column(name = "created_at")
+  @CreationTimestamp
+  private LocalDateTime createdAt;
 
   @OneToMany(cascade = CascadeType.ALL, mappedBy = "userRating")
   private Set<UserEventRating> userRatings = new HashSet<>();

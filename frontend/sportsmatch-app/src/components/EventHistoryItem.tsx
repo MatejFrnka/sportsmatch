cccdb2f8b66de05389dbcc259d217a78244c82b2
ivamach <ivana.machacikova@gmail.com>
2024-04-04 20:00:33 +0200

    SMA-74: added logic to determine macth status description in EventHistoryItem

--- frontend/sportsmatch-app/src/components/EventHistoryItem.tsx
+++ frontend/sportsmatch-app/src/components/EventHistoryItem.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react'
 import { EventHistoryDTO } from '../generated/api'
 import '../styles/EventHistoryItem.css'
 import Avatar from './Avatar'
@@ -7,12 +8,35 @@ interface EventHistoryProps {
 }
 
 function EventHistoryItem({ eventHistoryDTO }: EventHistoryProps) {
+  const [eventStatus, setEventStatus] = useState<string>('')
+
+  useEffect(() => {
+    const matchResult =
+      eventHistoryDTO.userScore! > eventHistoryDTO.opponentScore!
+        ? 'VICTORY'
+        : eventHistoryDTO.userScore! == eventHistoryDTO.opponentScore!
+          ? 'DRAW'
+          : 'LOSS'
+    if (eventHistoryDTO.status?.includes('MATCH')) {
+      setEventStatus(matchResult)
+    } else if (eventHistoryDTO.status?.includes('WAITING_FOR_RATING')) {
+      const result = matchResult + '(UNCONFIRMED)'
+      setEventStatus(result)
+    } else if (eventHistoryDTO.status?.includes('MISMATCH')) {
+      setEventStatus('SCORE MISMATCH')
+    }
+  }, [
+    eventHistoryDTO.opponentScore,
+    eventHistoryDTO.status,
+    eventHistoryDTO.userScore,
+  ])
+
   return (
     <>
       <div className="container-fluid">
         <div className={'row'}>
           <div className="event-history-item">
-            <div className="match-status">VICTORY (uncorfirmed)</div>
+            <div className="match-status">{eventStatus}</div>
             <div className="user-side">
               <div className="profile">
                 <div className="user-avatar">

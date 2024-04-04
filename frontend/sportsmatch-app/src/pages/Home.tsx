b5837ffd29d8db1a79ac783aeeedf179a711e2c3
ivamach <ivana.machacikova@gmail.com>
2024-04-04 20:09:47 +0200

    SMA-74: Home page connected to backend

--- frontend/sportsmatch-app/src/pages/Home.tsx
+++ frontend/sportsmatch-app/src/pages/Home.tsx
@@ -1,31 +1,66 @@
 import Match from '../components/Match'
-import SportEvent from '../components/SportEvent'
 import EventHistoryItem from '../components/EventHistoryItem'
+import { useEffect, useState } from 'react'
+import {
+  EventDTO,
+  EventHistoryDTO,
+  EventsControllerService,
+  ExSecuredEndpointService,
+  OpenAPI,
+  UserInfoDTO,
+} from '../generated/api'
 
 function Home() {
-  const sampleEvent = {
-    id: 1,
-    maxElo: 2000,
-    minElo: 1200,
-    dateEnd: '2024-05-02',
-    dateStart: '2024-05-01',
-    location: 'Prague, Stadium A',
-    title: 'Badminton match',
-    sport: 'Badminton',
-    playerOne: 'johndoe87',
-    playerTwo: 'jess_ward',
-  }
-  const sampleHistoryDTO = {
-    userScore: 1,
-    opponentScore: 2,
-    opponent: { name: 'Opponent' },
-  }
+  const page = 0
+  const size = 4
+  const sort = ['dateOfTheMatch,desc']
+  const [eventsHistory, setEventsHistory] = useState<EventHistoryDTO[]>([])
+  const [currentUser, setCurrentUser] = useState<UserInfoDTO>()
+  const [upcomingMatch, setUpcomingMatch] = useState<EventDTO[]>([])
+
+  useEffect(() => {
+    OpenAPI.TOKEN = localStorage.getItem('token')!
+    const init = async () => {
+      const fetchCurrentUser = await ExSecuredEndpointService.getUserMainPage()
+      if (fetchCurrentUser) {
+        setCurrentUser(fetchCurrentUser as UserInfoDTO)
+      }
+      if (currentUser && currentUser.sports) {
+        const sportIds = currentUser.sports.map((s) => s.id)
+        const fetchUpcomingEvents =
+          await EventsControllerService.getUpcomingEvents(sportIds as number[])
+        if (fetchUpcomingEvents) {
+          setUpcomingMatch(fetchUpcomingEvents as EventDTO[])
+        }
+      }
+    }
+    init()
+  }, [currentUser])
+
+  useEffect(() => {
+    OpenAPI.TOKEN = localStorage.getItem('token')!
+    const fetchEvents = async () => {
+      const response = await EventsControllerService.getEventsHistory(
+        //did not work with sorting, has to be checked
+        page,
+        size,
+        sort,
+      )
+      if (response && response.length > 0) {
+        setEventsHistory(response)
+      }
+    }
+    fetchEvents()
+  })
+
+  //TO DO: render ca three EventHistoryItems
+  //TO DO: add an alternative view when there are no past events
+  //TO DO: add an alternative view when there are no upcoming events
 
   return (
     <>
-      <Match event={sampleEvent} />
-      <SportEvent event={sampleEvent} />
-      <EventHistoryItem eventHistoryDTO={sampleHistoryDTO} />
+      <Match event={upcomingMatch[0]} />
+      <EventHistoryItem eventHistoryDTO={eventsHistory[0]} />
     </>
   )
 }

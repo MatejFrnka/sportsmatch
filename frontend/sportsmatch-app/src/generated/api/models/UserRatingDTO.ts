293da637aa9e7c3b5bed11a23d24e5065f5ed530
ivamach <ivana.machacikova@gmail.com>
2024-04-04 19:34:11 +0200

    SMA-74: updated generated api client files

---
+++ frontend/sportsmatch-app/src/generated/api/models/UserRatingDTO.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type UserRatingDTO = {
+    opponentName?: string;
+    userTextRating?: string;
+    userStarRating?: number;
+    createdAt?: string;
+};
+

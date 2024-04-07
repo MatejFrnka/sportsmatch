50d02bb5d5e11c51c023ae8ea40f47fc824926a5
ivamach <ivana.machacikova@gmail.com>
2024-04-07 17:59:46 +0200

    SMA-74: deleted getUpcomingMatch, added messages for no upcoming or pasr events

--- frontend/sportsmatch-app/src/pages/Home.tsx
+++ frontend/sportsmatch-app/src/pages/Home.tsx
@@ -13,7 +13,6 @@ import {
 function Home() {
   const page = 0
   const size = 4
-  const sort = ['dateOfTheMatch,desc']
   const [eventsHistory, setEventsHistory] = useState<EventHistoryDTO[]>([])
   const [currentUser, setCurrentUser] = useState<UserInfoDTO>()
   const [upcomingMatch, setUpcomingMatch] = useState<EventDTO[]>([])
@@ -22,20 +21,10 @@ function Home() {
     OpenAPI.TOKEN = localStorage.getItem('token')!
     const init = async () => {
       const fetchCurrentUser = await ExSecuredEndpointService.getUserMainPage()
-      if (fetchCurrentUser) {
-        setCurrentUser(fetchCurrentUser as UserInfoDTO)
-      }
-      if (currentUser && currentUser.sports) {
-        const sportIds = currentUser.sports.map((s) => s.id)
-        const fetchUpcomingEvents =
-          await EventsControllerService.getUpcomingEvents(sportIds as number[])
-        if (fetchUpcomingEvents) {
-          setUpcomingMatch(fetchUpcomingEvents as EventDTO[])
-        }
-      }
+      setCurrentUser(fetchCurrentUser as UserInfoDTO)
     }
     init()
-  }, [currentUser])
+  }, [])
 
   useEffect(() => {
     OpenAPI.TOKEN = localStorage.getItem('token')!
@@ -44,14 +33,14 @@ function Home() {
         //did not work with sorting, has to be checked
         page,
         size,
-        sort,
       )
       if (response && response.length > 0) {
         setEventsHistory(response)
+        console.log(response)
       }
     }
     fetchEvents()
-  })
+  }, [])
 
   //TO DO: render ca three EventHistoryItems
   //TO DO: add an alternative view when there are no past events
@@ -59,8 +48,17 @@ function Home() {
 
   return (
     <>
-      <Match event={upcomingMatch[0]} />
-      <EventHistoryItem eventHistoryDTO={eventsHistory[0]} />
+      {upcomingMatch.length > 0 ? (
+        <Match event={upcomingMatch[0]} />
+      ) : (
+        <div>No upcomnig events</div>
+      )}
+
+      {eventsHistory.length > 0 ? (
+        <EventHistoryItem eventHistoryDTO={eventsHistory[0]} />
+      ) : (
+        <div>No past Events</div>
+      )}
     </>
   )
 }

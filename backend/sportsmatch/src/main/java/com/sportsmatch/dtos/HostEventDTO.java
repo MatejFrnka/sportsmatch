d458bbf10ade87a92f5358025b5e7d8e4b1aa596
JanaDupcakova <jana.dupcakova@gmail.com>
2024-04-09 18:52:52 +0200

    created HostEventDTO

---
+++ backend/sportsmatch/src/main/java/com/sportsmatch/dtos/HostEventDTO.java
@@ -0,0 +1,23 @@
+package com.sportsmatch.dtos;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class HostEventDTO {
+  @NotNull private LocalDateTime dateStart;
+  @NotNull private LocalDateTime dateEnd;
+  @NotNull private Integer minElo;
+  @NotNull private Integer maxElo;
+  @NotBlank private String title;
+  private Long player1Id;
+  @NotBlank private String sport;
+  @NotNull private PlaceDTO placeDTO;
+}

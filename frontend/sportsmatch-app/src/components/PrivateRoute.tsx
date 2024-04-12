5739d1f76410de84c20ee7d2d8c34d2dd0ce6fb7
ivamach <ivana.machacikova@gmail.com>
2024-04-12 18:47:10 +0200

    SMA-80: updated PrivateRoute to check on the backend if user is logged in

--- frontend/sportsmatch-app/src/components/PrivateRoute.tsx
+++ frontend/sportsmatch-app/src/components/PrivateRoute.tsx
@@ -1,9 +1,29 @@
+import { useState, useEffect } from 'react'
 import { Navigate, Outlet } from 'react-router-dom'
+import { OpenAPI, ExSecuredEndpointService, ApiError } from '../generated/api'
 
 const PrivateRoute = () => {
   const requestedUrl = window.location.pathname
+  const [isLoggedIn, setLoggedIn] = useState<boolean>(false)
 
-  if (localStorage.getItem('token')) {
+  useEffect(() => {
+    const init = async () => {
+      OpenAPI.TOKEN = localStorage.getItem('token')!
+      try {
+        await ExSecuredEndpointService.getUserMainPage()
+        setLoggedIn(true)
+      } catch (error) {
+        const code = (error as ApiError).status        
+        if (code === 401 || code === 403) {
+          localStorage.removeItem('token')
+          setLoggedIn(false)
+        }
+      }
+    }
+    init()
+  }, [])
+
+  if (isLoggedIn) {
     return <Outlet />
   } else {
     return <Navigate to={'/login'} state={requestedUrl} />

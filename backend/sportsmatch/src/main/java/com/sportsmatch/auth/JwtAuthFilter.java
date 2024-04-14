cac12bdb8e4a97ef3479207b54c82b1cd8072b31
ivamach <ivana.machacikova@gmail.com>
2024-04-14 19:59:54 +0200

    SMA-80: removed unused imports

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtAuthFilter.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtAuthFilter.java
@@ -6,11 +6,9 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Arrays;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;

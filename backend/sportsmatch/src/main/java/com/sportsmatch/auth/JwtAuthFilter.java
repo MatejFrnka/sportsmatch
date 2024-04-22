aa14c2abdcb60e878104ea794bf8317bf28c0d8d
markpernia <perniamanila@gmail.com>
2024-04-22 12:02:34 +0200

    SMA-91: refactor jwtFilter added additional checks

--- backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtAuthFilter.java
+++ backend/sportsmatch/src/main/java/com/sportsmatch/auth/JwtAuthFilter.java
@@ -7,6 +7,8 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -15,8 +17,11 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+
+import static com.sportsmatch.auth.SecurityConfig.API_WHITE_LIST_URL;
 
 @Component
 @RequiredArgsConstructor
@@ -26,39 +31,58 @@ public class JwtAuthFilter extends OncePerRequestFilter {
   private final UserDetailsService userDetailsService;
   private final TokenRepository tokenRepository;
 
+  private final List<AntPathRequestMatcher> protectedPaths =
+      List.of(new AntPathRequestMatcher("/api/v1/**"));
+
   @Override
   protected void doFilterInternal(
-          @NonNull HttpServletRequest request,
-          @NonNull HttpServletResponse response,
-          @NonNull FilterChain filterChain)
-          throws ServletException, IOException {
+      @NonNull HttpServletRequest request,
+      @NonNull HttpServletResponse response,
+      @NonNull FilterChain filterChain)
+      throws ServletException, IOException {
 
     final String authHeader = request.getHeader("Authorization");
+    boolean requiresAuthentication = protectedPaths.stream().anyMatch(m -> m.matches(request));
+    boolean isWhitelisted =
+        Arrays.stream(API_WHITE_LIST_URL)
+            .anyMatch(path -> new AntPathRequestMatcher(path).matches(request));
 
-    if (isBearerTokenNotPresent(authHeader)) {
+    // Allow unsecured requests to pass through
+    if (!requiresAuthentication || isWhitelisted) {
       filterChain.doFilter(request, response);
       return;
     }
 
-    try {
-      final String jwt = authHeader.substring(7);
-      final String userEmail = jwtService.extractUserName(jwt);
-      UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+    if (isBearerTokenNotPresent(authHeader)) {
+      sendUnauthorizedError(response, "Token not present or expired token");
+      return;
+    }
 
-      if (isAuthenticationNeeded(userEmail) && jwtService.isTokenValid(jwt, userDetails)) {
-        if (!tokenRepository.existsByToken(jwt)) {
-          updateSecurityContext(request, userDetails);
-        }
-      } else {
-        sendUnauthorizedError(response, "Invalid or expired token");
-        return;
-      }
+    try {
+      handleJwtAuthentication(authHeader, request, response);
     } catch (JwtException e) {
       sendUnauthorizedError(response, "Invalid token");
       return;
     }
 
     filterChain.doFilter(request, response);
+  }
+
+  private void handleJwtAuthentication(
+      String authHeader, HttpServletRequest request, HttpServletResponse response)
+      throws IOException {
+    final String jwt = authHeader.substring(7);
+    final String userEmail = jwtService.extractUserName(jwt);
+    UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+    if (isAuthenticationNeeded(userEmail) && jwtService.isTokenValid(jwt, userDetails)) {
+      if (!tokenRepository.existsByToken(jwt)) {
+        updateSecurityContext(request, userDetails);
+      }
+    } else {
+      sendUnauthorizedError(response, "Invalid or expired token");
+      return;
+    }
   }
 
   public boolean isBearerTokenNotPresent(String authHeader) {
@@ -73,12 +97,13 @@ public class JwtAuthFilter extends OncePerRequestFilter {
 
   private void updateSecurityContext(HttpServletRequest request, UserDetails userDetails) {
     UsernamePasswordAuthenticationToken authToken =
-            new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+        new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
     authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
     SecurityContextHolder.getContext().setAuthentication(authToken);
   }
 
-  private void sendUnauthorizedError(HttpServletResponse response, String errorMessage) throws IOException {
+  private void sendUnauthorizedError(HttpServletResponse response, String errorMessage)
+      throws IOException {
     response.sendError(HttpServletResponse.SC_UNAUTHORIZED, errorMessage);
   }
 }

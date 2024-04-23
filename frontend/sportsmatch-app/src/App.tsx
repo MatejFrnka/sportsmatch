258e3504b37ec589c0deea3a9478fec5d89d19bd
MatejFrnka <mat.frnka@gmail.com>
2024-04-23 19:07:09 +0200

    SMA-101: added env variables for different environments (#80)

--- frontend/sportsmatch-app/src/App.tsx
+++ frontend/sportsmatch-app/src/App.tsx
@@ -14,6 +14,7 @@ import HostEvent from './pages/HostEvent'
 import { useEffect } from 'react'
 
 function App() {
+  OpenAPI.BASE = import.meta.env.VITE_BACKEND_URL
   OpenAPI.TOKEN = localStorage.getItem('token')!
 
   useEffect(() => {

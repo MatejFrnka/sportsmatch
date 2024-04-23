c0ee0782b5dfa372a46437bec43ae4449bcda051
JanaDupcakova <jana.dupcakova@gmail.com>
2024-04-23 19:59:51 +0200

    SMA 95 fixed check problem

--- backend/sportsmatch/src/test/java/com/sportsmatch/services/UserServiceImpTest.java
+++ backend/sportsmatch/src/test/java/com/sportsmatch/services/UserServiceImpTest.java
@@ -19,12 +19,8 @@ import java.util.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -43,9 +39,14 @@ class UserServiceImpTest extends BaseTest {
   private UserServiceImp userServiceImp;
 
   @BeforeEach
-  void initialize(){
-      userServiceImp = new UserServiceImp(userRepository, sportMapper, sportRepository,
-          new UserMapper(passwordEncoder, sportMapper),rankService);
+  void initialize() {
+    userServiceImp =
+        new UserServiceImp(
+            userRepository,
+            sportMapper,
+            sportRepository,
+            new UserMapper(passwordEncoder, sportMapper),
+            rankService);
   }
 
   @Test
